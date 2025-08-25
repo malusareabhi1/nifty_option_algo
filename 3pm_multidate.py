93bce185646bfebe8df3d882afde9586da2e8a71
Abhi Malusare <malusareabhi1@gmail.com>
2025-08-25 13:05:15 +0530

    Update 3pm_multidate.py

--- 3pm_multidate.py
+++ 3pm_multidate.py
@@ -1956,9 +1956,9 @@ for i in range(1, len(unique_days)):
     # Call your trading signal function
     #signal = trading_signal_all_conditions1(day_df)
     #
-    #signal = trading_signal_all_conditions2(day_df) 
+    signal = trading_signal_all_conditions2(day_df) 
 
-    signal = trading_signal_all_conditions_2_improved(day_df) 
+    #signal = trading_signal_all_conditions_2_improved(day_df) 
     
 #######################################################################################
     
