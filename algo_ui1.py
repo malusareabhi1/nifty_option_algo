e35e1ef913293f039eb0eb191d3ac7a1e5d136c5
Abhi Malusare <malusareabhi1@gmail.com>
2025-09-02 18:41:24 +0530

    Update algo_ui1.py

--- algo_ui1.py
+++ algo_ui1.py
@@ -421,7 +421,7 @@ elif MENU == "Dashboard":
         st.subheader("Live NIFTY 50 Market Data")
 
         # Predefined NIFTY50 stock list (you can fetch dynamically from NSE if needed)
-        nifty50_symbols = ["NIFTY","BANKNIFTY",
+        nifty50_symbols = ["^NSEI","^NSEBANK",
             "RELIANCE.NS", "TCS.NS", "INFY.NS", "HDFCBANK.NS", "ICICIBANK.NS", "KOTAKBANK.NS",
             "AXISBANK.NS", "SBIN.NS", "ITC.NS", "LT.NS", "BHARTIARTL.NS", "ASIANPAINT.NS",
             "HINDUNILVR.NS", "MARUTI.NS", "BAJFINANCE.NS", "HCLTECH.NS", "ULTRACEMCO.NS",
