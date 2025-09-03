50c146a540fd74dd984d3075357d6764918ea278
Abhi Malusare <malusareabhi1@gmail.com>
2025-09-03 15:34:11 +0530

    Update algo_ui1.py

--- algo_ui1.py
+++ algo_ui1.py
@@ -636,7 +636,7 @@ elif MENU == "Dashboard":
             st.write(df)
             # Remove timezone if exists
             df['Datetime'] = df['Datetime'].dt.tz_localize(None)
-        
+            st.write(df)
             # Extract date
             df['Date'] = df['Datetime'].dt.date
             unique_days = sorted(df['Date'].unique())
