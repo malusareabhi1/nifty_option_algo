f512bd83f622d6bf1bed45149cc0d3d6bdd93ead
Abhi Malusare <malusareabhi1@gmail.com>
2025-09-03 18:22:46 +0530

    Update ui_dash.py

--- ui_dash.py
+++ ui_dash.py
@@ -395,7 +395,7 @@ elif MENU == "Backtest":
             (s for s in st.session_state.strategies if s["name"] == st.session_state.selected_strategy),
             None
         )
-
+        st.divider()
         # Show description in markdown
         if selected and "description" in selected:
             st.markdown(f"### Strategy Info\n{selected['description']}")
