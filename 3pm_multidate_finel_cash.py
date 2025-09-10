b643406a3dc76cc86757d0d5cea6dbd23007b9c0
Abhi Malusare <malusareabhi1@gmail.com>
2025-09-10 23:44:44 +0530

    Update 3pm_multidate_finel_cash.py

--- 3pm_multidate_finel_cash.py
+++ 3pm_multidate_finel_cash.py
@@ -412,7 +412,7 @@ for i in range(1, len(unique_days)):
 
     # Call your trading signal function
     signal = trading_signal_equity(day_df)
-st.write(signal)
+    st.write(signal)
 #st.write(signal)
 # 2. Fetch option premium data for that strike
 #option_df = get_option_data_realtime(signal['spot_price'], signal['expiry'], signal['strike'])  # 15m candles
