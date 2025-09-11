139f1b3609c7d00b5183c05cc1d7d98c14fc9997
Abhi Malusare <malusareabhi1@gmail.com>
2025-09-11 10:46:17 +0530

    Update 3pm_multidate_finel_cash.py

--- 3pm_multidate_finel_cash.py
+++ 3pm_multidate_finel_cash.py
@@ -87,7 +87,7 @@ def trading_signal_equity(df, quantity=100, return_all_signals=False):
     def monitor_trade(sig):
         current_sl = sig.get('stoploss')
         entry_dt = sig['entry_time']
-        exit_deadline = entry_dt + timedelta(minutes=20)
+        exit_deadline = entry_dt + timedelta(minutes=25)
 
         if day1_after_915.empty:
             sig['exit_price'] = sig['entry_price']
