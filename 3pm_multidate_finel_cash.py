69b592c8d50b4127fde8bc602eb709bb9e60c37e
Abhi Malusare <malusareabhi1@gmail.com>
2025-09-11 10:28:16 +0530

    Update 3pm_multidate_finel_cash.py

--- 3pm_multidate_finel_cash.py
+++ 3pm_multidate_finel_cash.py
@@ -413,7 +413,17 @@ for i in range(1, len(unique_days)):
     # Call your trading signal function
     signal = trading_signal_equity(day_df)
     #st.write(signal.table)
-    st.write(pd.DataFrame([signal]))
+    #st.write(pd.DataFrame([signal]))
+    if signal:   # append only if signal generated
+        trade_log.append(signal)
+
+# ✅ Convert to DataFrame once and display full log
+if trade_log:
+    trade_log_df = pd.DataFrame(trade_log)
+    st.subheader("Trade Log")
+    st.dataframe(trade_log_df, use_container_width=True)
+else:
+    st.info("No trades found in the selected period.")
 #st.write(signal)
 # 2. Fetch option premium data for that strike
 #option_df = get_option_data_realtime(signal['spot_price'], signal['expiry'], signal['strike'])  # 15m candles
