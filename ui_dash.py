1f9d7abaf7798e6226492da0a1a72d5173c6b27b
Abhi Malusare <malusareabhi1@gmail.com>
2025-11-20 20:13:16 +0530

    Update ui_dash.py

--- ui_dash.py
+++ ui_dash.py
@@ -944,9 +944,9 @@ with st.sidebar:
     st.markdown(f"<div style='display:none' data-theme={'dark' if st.session_state.theme_dark else 'light'}></div>", unsafe_allow_html=True)
 
     #st.image(
-        "https://assets-global.website-files.com/5e0a1f0d3a9f1b6f7f1b6f34/5e0a1f63a4f62a5534b5f5f9_finance-illustration.png",
-        use_container_width=True,
-    )
+        #"https://assets-global.website-files.com/5e0a1f0d3a9f1b6f7f1b6f34/5e0a1f63a4f62a5534b5f5f9_finance-illustration.png",
+        #use_container_width=True,
+    #)
 
     MENU = st.radio(
         "Navigate",
